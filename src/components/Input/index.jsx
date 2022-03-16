11765a868db3431058f87fac833d6110914a6386
leonardoliska <leo.liska@hotmail.com>
2022-03-16 16:16:53 -0300

    feat: add styling to input component

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,7 +1,12 @@
 import { Container } from "./styles";
 
 const Input = ({ label, register, name, error, ...rest }) => {
-  return <Container {...rest} />;
+  return (
+    <Container>
+      <label>{label}</label>
+      <input {...rest} />
+    </Container>
+  );
 };
 
 export default Input;
