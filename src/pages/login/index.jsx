10a06c2de4a3f2b5902ee08d575ea558f2be41c9
leonardoliska <leo.liska@hotmail.com>
2022-03-17 02:26:04 -0300

    feat: add login form validation

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,26 +1,67 @@
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+
 import LoginImage from "../../assets/login_icon.png";
 import Container from "./styles";
 import Button from "../../components/button";
 import Input from "../../components/Input";
+import NavBar from "../../components/NavBar";
 
 function Login() {
+  const formSchema = yup.object().shape({
+    email: yup.string().required("Email obrigatório").email("Email Inválido"),
+    password: yup
+      .string()
+      .required("Senha obrigatória")
+      .min(8, "A senha deve ter no mínimo 8 caractéres"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <Container>
-      <section>
-        <figure>
-          <img src={LoginImage} alt="Imagem com três pessoas juntas" />
-          <span>Bem vindo de volta!</span>
-          <span>
-            Efetue seu <span>Login</span>
-          </span>
-        </figure>
-        <form>
-          <Input label="Email" placeholder="Email" />
-          <Input label="Senha" placeholder="Senha" type="password" />
-          <Button>Login</Button>
-        </form>
-      </section>
-    </Container>
+    <>
+      <NavBar />
+      <Container>
+        <section>
+          <figure>
+            <img src={LoginImage} alt="Imagem com três pessoas juntas" />
+            <span>Bem vindo de volta!</span>
+            <span>
+              Efetue seu <span>Login</span>
+            </span>
+          </figure>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Input
+              label="Email"
+              name="email"
+              placeholder="Email"
+              register={register}
+              error={errors?.email}
+            />
+            <Input
+              label="Senha"
+              name="password"
+              placeholder="Senha"
+              type="password"
+              register={register}
+              error={errors?.password}
+            />
+            <Button>Login</Button>
+          </form>
+        </section>
+      </Container>
+    </>
   );
 }
 
