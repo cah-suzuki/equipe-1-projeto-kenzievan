c12ae6586dc648c7ac546cbaebf3b4c194b2e029
camila suzuki <camila.suzuki.p@gmail.com>
2022-03-18 07:31:13 -0300

    feat: component studentList created

---
+++ src/components/StudentList/style.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 70%;
+  border: solid orange 1px;
+  display: flex;
+  flex-direction: column;
+  padding: 22px;
+  font-family: "Nunito", sans-serif;
+  border-radius: 10px;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    font-size: 24px;
+
+    button {
+      /* background: var(--color-primary-100); */
+      background-color: #fa8223;
+      color: white;
+      height: 65px;
+      width: 190px;
+    }
+
+    h2 {
+      font-size: 24px;
+    }
+
+    ul {
+      list-style-type: none;
+      display: flex;
+      flex-direction: column;
+      button {
+        border: solid orange 1px;
+        border-radius: 10px;
+        margin-bottom: 12px;
+        width: 96%;
+        height: 70px;
+        padding: 12px;
+        }
+      }
+    }
+  }
+`;
