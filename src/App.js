4e170776812df9d358bcd81e55374133d3eabf6a
Paulo Guarnieri <paulo.r.guarnieri@hotmail.com>
2022-03-20 16:42:54 -0300

    style: cleaning empty spaces in the code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import GlobalStyle from "./styles/global";
 import Routes from "../src/routes";
 import Button from "./components/button";
-import DriverDashboard from "./pages/driverDashboard";
 
 function App() {
   return (
     <>
-      <DriverDashboard />
       <GlobalStyle />
-      {/* <Routes /> */}
+      <Routes />
     </>
   );
 }
