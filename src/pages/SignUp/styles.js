7dc61b2503397741a4ecef6eb7749660527c6af2
leonardoliska <leo.liska@hotmail.com>
2022-03-20 16:46:20 -0300

    fix: remove fixed signup form height

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -2,9 +2,8 @@ import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
-  padding: 0 5%;
+  padding: 0 5% 25px;
   width: 100%;
-  height: 100%;
   justify-content: center;
   align-items: center;
   gap: 40px;
