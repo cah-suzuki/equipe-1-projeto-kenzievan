e485574bcd5011e773d3e27fc9495989a3ee64d7
Paulo Guarnieri <paulo.r.guarnieri@hotmail.com>
2022-03-20 16:39:51 -0300

    feat create DriverDashboard containers and organization

--- src/pages/driverDashboard/index.jsx
+++ src/pages/driverDashboard/index.jsx
@@ -1,5 +1,26 @@
+import { LeftContainer, RightContainer, MainContainer, PageContainer } from './styles';
+import NavBar from '../../components/NavBar';
+import StudentList from '../../components/StudentList';
+import MessagesList from '../../components/messagesList';
+import GroupMessages from '../../components/groupMessages';
+// import Header from '../../components/header';
+
 function DriverDashboard() {
-  return <h1>DRIVER DASHBOARD PAGE</h1>;
+  return (
+    <PageContainer>
+      <NavBar />
+      {/* <Header /> */}
+      <MainContainer>
+        <LeftContainer>
+          <StudentList />
+        </LeftContainer>
+        <RightContainer>
+          <GroupMessages />
+          <MessagesList />
+        </RightContainer>
+      </MainContainer>
+    </PageContainer>
+  )
 }
 
 export default DriverDashboard;
