267b006e2255f60d0bc536909a5df07782d0de31
Daniele Calixto <danielecalixtob@gmail.com>
2022-03-21 09:29:15 -0300

    style: initial styling of buttonSmall

--- src/components/buttonSmall/styled.js
+++ src/components/buttonSmall/styled.js
@@ -4,7 +4,7 @@ export const Container = styled.button`
   background-color: var(--color-primary-100);
   width: 30px;
   height: 30px;
-  border-radius: 5px;
+  border-radius: 50%;
   font-size: 25px;
   color: var(--white);
 `;
