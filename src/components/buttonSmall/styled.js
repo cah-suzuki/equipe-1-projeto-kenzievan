59dc108970dda1aeaddcbb4601e5a919d9d493d3
Daniele Calixto <danielecalixtob@gmail.com>
2022-03-21 18:55:05 -0300

    style: style buttonSmall

--- src/components/buttonSmall/styled.js
+++ src/components/buttonSmall/styled.js
@@ -1,10 +1,15 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  align-content: center;
   background-color: var(--color-primary-100);
   width: 30px;
   height: 30px;
-  border-radius: 50%;
-  font-size: 25px;
+  border: none;
+  border-radius: ${(props) => (props.isSquare ? "7px" : "50%")};
+  font-size: 22px;
   color: var(--white);
 `;
