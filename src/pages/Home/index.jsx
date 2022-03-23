147911e00a2b5298856824631f9b83279a95a915
camila suzuki <camila.suzuki.p@gmail.com>
2022-03-23 05:49:25 -0300

    style: update in main text and team members photo and link to github created

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,5 +1,11 @@
 import { Contanier, Main, Footer, SideBackground } from "./styles";
-import TeamIcon from "../../assets/TeamIcon.png";
+
+import camilaIcon from "../../assets/img/camilaIcon.png";
+import daniIcon from "../../assets/img/daniIcon.png";
+import leoIcon from "../../assets/img/leoIcon.png";
+import marcioIcon from "../../assets/img/marcioIcon.png";
+import pauloIcon from "../../assets/img/pauloIcon.png";
+import tiagoIcon from "../../assets/img/tiagoIcon.png";
 
 // import SideBackground from "../../components/SideBackground";
 import SideImage from "../../assets/SideImage.svg";
@@ -12,21 +18,26 @@ function Home() {
       <Main>
         {/* <div> */}
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          labore consequuntur aspernatur. Fugit, distinctio molestias culpa
-          cumque neque vel dolores.
+          KenzieVan é uma plataforma de comunicação entre pais e responsáveis e
+          motoristas de vans escolares.
         </p>
         <SideBackground image={SideImage} />
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          labore consequuntur aspernatur. Fugit, distinctio molestias culpa
-          cumque neque vel dolores.
+          Com kenzieVan você pode comunicar sobre faltas e atrasos de forma
+          simples e evitar qualquer stress durante a viagem
         </p>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
-          labore consequuntur aspernatur. Fugit, distinctio molestias culpa
-          cumque neque vel dolores.
+          Principais Recursos:
+          <ol>
+            <li>Encontrar a van de forma rápida e fácil</li>
+            <li> Acesso ao histórico de viagens</li>
+            <li> Chat entre responsável e motorista</li>
+            <li>Histórico de mensagens</li>
+            <li> Visualização rápida de dados dos alunos </li>
+            <li> Layout intuitivo e amigável</li>
+          </ol>
         </p>
+        <p>Cadastre-se e começe a usar hoje!</p>
         {/* </div> */}
       </Main>
       <Footer>
@@ -35,12 +46,66 @@ function Home() {
         <div>
           <h3>Meet the Team</h3>
           <div>
-            <img src={TeamIcon} alt="asdasd" />
-            <img src={TeamIcon} alt="asdasd" />
-            <img src={TeamIcon} alt="asdasd" />
-            <img src={TeamIcon} alt="asdasd" />
-            <img src={TeamIcon} alt="asdasd" />
-            <img src={TeamIcon} alt="asdasd" />
+            <a
+              href="https://github.com/tiagomgm"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={tiagoIcon}
+                alt="ícone com foto do dev frontend tiago mota ao clicar leva ao github do mesmo"
+              />
+            </a>
+            <a
+              href="https://github.com/danielecalixto"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={daniIcon}
+                alt="ícone com foto da dev frontend daniele calixto ao clicar leva ao github da mesma"
+              />
+            </a>
+            <a
+              href="https://github.com/marcio-tonholo"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={marcioIcon}
+                alt="ícone com foto do dev frontend marcio jose ao clicar leva ao github do mesmo"
+              />
+            </a>
+            <a
+              href="https://github.com/leonardoliska"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={leoIcon}
+                alt="ícone com foto do dev leonardo liska ao clicar leva ao github do mesmo"
+              />
+            </a>
+            <a
+              href="https://github.com/pauloguarnieri"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={pauloIcon}
+                alt="ícone com foto do dev frontend paulo guarnieri ao clicar leva ao github do mesmo"
+              />
+            </a>
+            <a
+              href="https://github.com/cah-suzuki"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={camilaIcon}
+                alt="ícone com foto da dev frontend camila suzuki ao clicar leva ao github da mesma"
+              />
+            </a>
           </div>
         </div>
       </Footer>
